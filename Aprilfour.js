a67e29f10aeedd0f8387bf3f235a32e1052c9772
sasikumar3123 <Sasilearner31@gmail.com>
2023-04-26 11:55:50 +0400

    Thirumbavum aramikran scratch lerundhu

--- Aprilfour.js
+++ Aprilfour.js
@@ -1,5 +1,23 @@
 function myfunction()
 {
-	document.getElementById('heading').innerHTML = "Changed";
-	window.print();
+	let user = prompt("who's there",'');
+if(user == "Admin"){
+    let pwd = prompt("enter password");
+    if(pwd == 123){
+        alert("Welcome");
+    }
+    else if(pwd == null|| pwd == ''){
+        alert("Cancelled");
+    }
+    else
+    {
+        alert("wrong password");
+    }
+}
+else if(user == null||user == ''){
+    alert("Cancelled");
+}
+else{
+    alert("I don't know you!");
+}
 }
